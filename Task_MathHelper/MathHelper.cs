17e486eab41cf75a7493d3923aeac53330408030
Kamran Z <kamraneze@code.edu.az>
2023-08-16 11:28:54 +0400

    Task Math Helper Completed

---
+++ Task_MathHelper/MathHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_MathHelper
+{
+    internal class MathHelper
+    {
+
+        public int PrintValue(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int PrintValue(bool number)
+        {
+            if (number) return 1;
+            return 0;
+        }
+        public void PrintValue(string name, string surname)
+        {
+            Console.WriteLine(name + " " + surname);
+        }
+        public int PrintValue(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+
+
+    }
+}
---
+++ Task_MathHelper/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_MathHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper math = new();
+            var a = math.PrintValue(4, 5);
+            Console.WriteLine(a);
+
+            var b = math.PrintValue(false);
+            Console.WriteLine(b);
+
+            var c = math.PrintValue(3, 4, 5);
+            Console.WriteLine(c);
+
+            math.PrintValue("Kamran", "Zeynalov");
+        }
+    }
+}
